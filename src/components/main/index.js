2b8fc0968a03cc58d44909f43b02e2dd3c1aa309
PeterChauYEG <peter.chau.esq@gmail.com>
2018-06-09 08:41:21 -0600

    set up skeleton for leaderboard page

---
+++ src/components/main/index.js
@@ -0,0 +1,79 @@
+// Main React component, that we'll import in `src/app.js`
+//
+// Note a few points from this file:
+//
+// 1.  We're using the format `main/index.js` for this file, which means we
+// can simply import 'src/components/main', which will auto-default to index.js.
+// This is a useful pattern when you have styles/images to pull from, and you
+// want to keep the component tree organised.
+//
+// 2.  We import SASS and a logo SVG file directly.  Classnames will be hashed
+// automatically, and images will be compressed/optimised in production.  File
+// names that are made available in the import variable will be identical on
+// both the server and browser.
+//
+// 3.  We're introducing React Router in this component.  In RR v4, routes are
+// not defined globally-- they're defined declaratively on components, so we
+// can respond to route changes anywhere.
+//
+// 4.  We're using `react-helmet`, which allows us to set <head> data like
+// a <title> or <meta> tags, which are filtered up to the main <Html> component
+// before HTML rendering.
+
+// ----------------------
+// IMPORTS
+
+/* NPM */
+
+// React
+import React from 'react';
+
+// Routing via React Router
+import {
+  Link,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
+// <Helmet> component for setting the page title/meta tags
+import Helmet from 'react-helmet';
+
+/* ReactQL */
+
+// NotFound 404 handler for unknown routes, and the app-wide `history` object
+// we can use to make route changes from anywhere
+import { Redirect, history } from 'kit/lib/routing';
+
+/* App */
+
+// Child React components. Note:  We can either export one main React component
+// per file, or in the case of <Home>, <Page> and <WhenFound>, we can group
+// multiple components per file where it makes sense to do so
+import GraphQLMessage from 'components/graphql';
+import { Home, WhenNotFound } from 'components/routes';
+
+// Styles
+import css from './main.scss';
+
+// ----------------------
+
+// Example function to show that the `history` object can be changed from
+// anywhere, simply by importing it-- use this in Redux actions, functions,
+// React `onClick` events, etc.
+function changeRoute() {
+  history.push('/page/about');
+}
+
+export default () => (
+  <div>
+    <Helmet>
+      <title>Twitch Game</title>
+      <meta name="description" content="A Twitch based puzzle arena game" />
+      {/* <base href="http://localhost:8081/" /> */}
+    </Helmet>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route component={WhenNotFound} />
+    </Switch>
+  </div>
+);
---
+++ src/components/routes.js
@@ -0,0 +1,53 @@
+// Demonstrates several components on one page, each with their own `export`.
+//
+// These are smaller components that <Main> imports, and changes depending
+// on the page route (via React Router).
+//
+// <WhenNotFound> demonstrates the use of <NotFound>, a ReactQL helper
+// component that signals to our web server that we have a 404 error, to handle
+// accordingly
+
+// ----------------------
+// IMPORTS
+
+/* NPM */
+
+// React
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/* ReactQL */
+
+// NotFound 404 handler for unknown routes
+import { NotFound } from 'kit/lib/routing';
+
+// ----------------------
+
+// We'll display this <Home> component when we're on the / route
+export const Home = () => (
+  <div>
+    <h1>Twitch Game Leader Board</h1>
+    <hr />
+    <p>Game Start: 12:00 Mountain Time, June 11, 2018</p>
+    <p>Game End: 20:00 Mountain Time, June 11, 2018</p>
+    <p>Time Remaining: 8:00 Hours</p>
+    <hr />
+    <p>Clues:</p>
+    <p>What is love?</p>
+    <hr />
+    <p>Game Winner:</p>
+    <p>Laboratory One</p>
+    <hr />
+    <p>Game Rankings:</p>
+    <p>1. Laboratory One - 100 points</p>
+    <p>2. Northwest - 1 point</p>
+    <p>3. YungTuxedoMask - 1 point</p>
+  </div>
+);
+
+// Create a route that will be displayed when the code isn't found
+export const WhenNotFound = () => (
+  <NotFound>
+    <h1>Unknown route - the 404 handler was triggered!</h1>
+  </NotFound>
+);
